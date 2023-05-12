e445d644faf8baf8cf9afc9d20b2ab306a1f6fb0
hyllow <el02052003@gmail.com>
2023-05-12 19:10:08 +0100

    fd number

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -11,3 +11,20 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void ft_putnbr_fd(int n , int fd)
+{
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n -= 1;
+		ft_putnbr_fd(n , fd);
+	}
+	if ( n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else if (n >= 0 && n <= 9)
+		ft_putchar_fd(n + 48, fd);
+}
